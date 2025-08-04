be0fba646366ff1807583e9e1d55244f7d2aa322
Udaykumar1677 <udaykumarvemavarapu1677@gmail.com>
2025-08-04 17:53:30 +0530

    Fix init_db and add requirements.txt

--- app.py
+++ app.py
@@ -379,21 +379,27 @@ def delete_pdf(filename):
 
 
 
-def init_db():
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL
-        )
-    ''')
-    conn.commit()
-    conn.close()
+import os
+import sqlite3
 
-# Run only once when the server starts
+def init_db():
+    if not os.path.exists("users.db"):
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
+        conn.close()
+        print("✅ Database initialized.")
+    else:
+        print("⚠️ Database already exists.")
+
 init_db()
 
 
